7bbcecf605885d86272ef7b2b1dc75d9343942be
Preston Abraham <112017483+preston-abraham@users.noreply.github.com>
2023-12-06 08:12:37 -0600

    Refactor Day 6 to remove unnecessary lines

--- Solutions/Day6.jl
+++ Solutions/Day6.jl
@@ -5,39 +5,18 @@ function travel(time,hold_time)
     return (time - hold_time) * hold_time
 end
 
-tot = 1
-for i in range(1,4)
+function winning_strats(time,distance)
     values = []
-    for ht in range(1,times[i])
-        push!(values,travel(times[i],ht))
+    for ht in range(1,time)
+        push!(values,travel(time,ht))
     end
-    filter!(e->e>distances[i],values)
-    tot *= size(values)[1]
+    filter!(e->e>distance,values)
+    return(size(values)[1])
 end
-print(String("Part 1: $(tot)"))
+
+print(String("Part 1: $(prod([winning_strats(times[i],distances[i]) for i in range(1,4)]))"))
 
 time = 53897698
 distance = 313109012141201
 
-# Binary Search Start
-start = 1
-while travel(time,start) <= distance
-    start *= 2
-end
-while travel(time,start) > distance
-    start -= 1
-end
-start = start + 1
-
-# Binary Search Last
-last = time
-while travel(time,last) <= distance
-     last /= 2
-end
-while travel(time,last) > distance
-    last += 1
-end
-last = last - 1
-
-# Get Range
-print(String("Part 2: $(last - start + 1)"))
+print(String("\nPart 2: $(winning_strats(time,distance))"))
