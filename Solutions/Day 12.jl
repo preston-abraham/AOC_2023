75982369314ccbcd5b7291d7a4b9a959058ee2ff
Preston Abraham <112017483+preston-abraham@users.noreply.github.com>
2023-12-13 09:35:46 -0600

    Update file with day12 recursive re-write

--- Solutions/Day 12.jl
+++ Solutions/Day 12.jl
@@ -1,3 +1,4 @@
+# My Original Attempt
 input = open(io->read(io, String), "inputs/day12.txt")
 lines = split(sample_input,"\n")
 seqs = [split(split(i," ")[2],",") for i in lines]
@@ -92,3 +93,57 @@ for i in 1:size(expanded_springs)[1]
     tot += valid_options(expanded_springs[i],expanded_seqs[i])
 end
 tot
+
+##############################################################################################################################################
+
+# Day 12 Re-write
+#        (Treat as completely separate code from above)
+using Memoization
+input = open(io->read(io, String), "inputs/day12.txt")
+lines = split(input,"\n")
+seqs = [split(split(i," ")[2],",") for i in lines]
+springs = [split(split(i," ")[1],"") for i in lines]
+
+function expand_seq(seq,use_separator,separator)
+    new_seq = copy(seq)
+    for i in 1:4
+        if use_separator
+            new_seq = vcat(new_seq,separator,seq)
+        else
+            new_seq = vcat(new_seq,seq)
+        end
+    end
+    return new_seq
+end
+
+@memoize function recursive_solve(spring,seq,consecutive=0)
+    if size(spring)[1] == 0
+        if (size(seq)[1] == 0 && consecutive == 0)
+            return 1
+        elseif (size(seq)[1] == 1 && consecutive == parse(Int64,seq[1]))
+            return 1
+        else
+            return 0
+        end
+    end
+
+    count = 0
+    condition = spring[1]
+    if condition ∈ ["#","?"]
+        count += recursive_solve(spring[2:end],seq,consecutive+1)
+    end
+    if condition ∈ [".","?"]
+        if size(seq)[1] > 0 && parse(Int64,seq[1]) == consecutive
+            count += recursive_solve(spring[2:end],seq[2:end])
+        elseif consecutive == 0
+            count += recursive_solve(spring[2:end],seq)
+        end
+    end
+    return count
+end
+
+expanded_springs = expand_seq.(springs,true,["?"])
+expanded_seqs = expand_seq.(seqs,false,[","])
+
+print(String("Part 1: $(sum([recursive_solve(vcat(springs[i],["."]),seqs[i],0) for i in 1:size(springs)[1]]))"))
+print(String("\nPart 2: $(sum([recursive_solve(vcat(expanded_springs[i],["."]),expanded_seqs[i],0) for i in 1:size(expanded_springs)[1]]))"))
